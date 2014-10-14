563e88d29502a114a5405022c46d88dde1ec2e34
pavlov <pavlov@pavlov-lviv-pc.(none)>
2014-10-14 10:58:56 +0300

    Separate Lead and Visitor API
    Add tests which covers create methods

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   namespace :api do
     namespace :v1 do
-      resources :leads
+      with_options only: :create do |option|
+        option.resources :leads
+        option.resources :visitors
+      end
     end
   end
   # Example of regular route:
