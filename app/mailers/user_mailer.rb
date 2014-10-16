3bf2e4cf1bb1cf4f4d4610d7765dd71e2ea32a42
pavlov <pavlov@pavlov-lviv-pc.(none)>
2014-10-16 17:19:26 +0300

    Add bcc to Erik, change template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,12 @@ class UserMailer
 
   def lead_creating(lead)
     template 'lead-was-created'
+    subject = "Pet-Insurance.org New Lead - #{lead.id} #{lead.created_at} "
     set_template_values(set_lead_params(lead))
-    mail to: wrap_recipient(ENV["RECIPIENT_EMAIL"], ENV["RECIPIENT_NAME"])
+
+    mail to: [wrap_recipient(ENV["RECIPIENT_EMAIL"], ENV["RECIPIENT_NAME"], "to"),
+              wrap_recipient(ENV["RECIPIENT_BCC_EMAIL"], ENV["RECIPIENT_BCC_NAME"], "bcc")], subject:subject
+
   end
 
   private
@@ -14,13 +18,16 @@ class UserMailer
     {
      first_name: lead.first_name,
      last_name: lead.last_name,
-     zip: lead.zip,
+     email: lead.email,
      day_phone: lead.day_phone,
-     email: lead.email
+     zip: lead.zip,
+     pet_name: lead.details_pets.first.pet_name,
+     species: lead.details_pets.first.species,
+     breed: lead.details_pets.first.breed
     }
   end
 
-  def wrap_recipient(email, name)
-    [{ email: email, name: name }]
+  def wrap_recipient(email, name, type)
+    { email: email, name: name, type: type }
   end
 end
