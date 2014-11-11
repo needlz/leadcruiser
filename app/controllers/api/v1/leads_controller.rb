fbf1cef52d4b105b77aa023d3e440a6232e0534b
rorfan8782 <knhsinbad8782@gmail.com>
2014-11-11 15:36:17 +0100

    Complete round robin request

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -2,7 +2,6 @@ require 'data_generators/pet_premium_generator'
 require 'data_generators/pet_first_generator'
 require 'next_client_builder'
 require 'data_generator_provider'
-require 'data_generator_provider_json'
 require 'workers/send_data_worker.rb'
 class API::V1::LeadsController  < ActionController::API
 
@@ -12,10 +11,10 @@ class API::V1::LeadsController  < ActionController::API
 
     if lead.save
       render json: { message: 'Lead was created successfully' }, status: :created
-      client_verticals = ClientsVertical.where(vertical_id: lead.vertical_id, active: true, exclusive: true).order('id')
-      builder = NextClientBuilder.new(lead, client_verticals)
-      SendDataWorker.perform_async(lead.id, builder)
-      # SendDataWorker.new.perform(lead.id, builder)
+      # client_verticals = ClientsVertical.where(vertical_id: lead.vertical_id, active: true, exclusive: true)
+      # builder = NextClientBuilder.new(lead, client_verticals)
+      # SendDataWorker.perform_async(lead.id, builder.integration_name)
+      SendDataWorker.new.perform(lead.id)
     else
       render json: { errors: lead.error_messages + pet.error_messages }, status: :unprocessable_entity
     end
--- lib/workers/send_data_worker.rb
+++ lib/workers/send_data_worker.rb
@@ -1,41 +1,42 @@
 require 'workers/response_petfirst_worker.rb'
 
 class SendDataWorker
+
+  attr_accessor :client
+
   include Sidekiq::Worker
   sidekiq_options queue: "high"
-  def perform(lead_id, builder)
-    return unless builder
+  def perform(lead_id)
+    return unless lead_id
 
     lead = Lead.find(lead_id)
-    builder.client_verticals.each do |client|
-
+    client_verticals = ClientsVertical.where(vertical_id: lead.vertical_id, active: true, exclusive: true)
+    
+    response = nil
+    count = 0    
+    while response.nil? do
+      if count == client_verticals.count
+        break
+      end
+      builder = NextClientBuilder.new(lead, client_verticals)
+      @client = ClientsVertical.find_by_integration_name(builder.integration_name)
       provider = DataGeneratorProvider.new(lead, client)
 
       response = provider.send_data
       binding.pry
-      unless response.nil?
+
+      count += 1
+    end
+
+    unless response.nil?
+      binding.pry
+      Response.create(response: response.to_s, lead_id: lead.id)
+      if @client.integration_name == "pet_first"
         binding.pry
-        Response.create(response: response.to_s, lead_id: lead.id)
-        if client.integration_name == "pet_first"
-          binding.pry
-          ResponsePetfirstWorker.perform_async(lead_id)
-        end
-        break
+        # ResponsePetfirstWorker.perform_async(lead_id)
+        ResponsePetfirstWorker.new.perform(lead_id)
       end
     end
-    # provider = DataGeneratorProvider.new(lead, integration_name)
-    # response = provider.send_data
-    # if response.nil?
-    #   provider = DataGeneratorProviderJson.new(lead, "pet_first")
-    #   response = provider.send_data
 
-    #   if response.nil?
-    #     ResponsePetfirstWorker.perform_async(lead_id)
-    #   end
-    # end
-
-    # unless response.nil?
-    #   Response.create(response: response.to_s, lead_id: lead.id)
-    # end
   end
 end
