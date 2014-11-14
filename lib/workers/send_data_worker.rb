9e4fcd2e532c17e137275b76469538fe95cd1bf3
rorfan8782 <knhsinbad8782@gmail.com>
2014-11-14 21:06:04 +0100

    Modified round-robin selling

--- lib/workers/send_data_worker.rb
+++ lib/workers/send_data_worker.rb
@@ -13,8 +13,9 @@ class SendDataWorker
     client_verticals = ClientsVertical.where(vertical_id: lead.vertical_id, active: true, exclusive: true)
     
     response = nil
+    sold = false
     count = 0    
-    while response.nil? do
+    while !sold do
       if count == client_verticals.count
         break
       end
@@ -23,11 +24,25 @@ class SendDataWorker
       provider = DataGeneratorProvider.new(lead, client)
 
       response = provider.send_data
+      unless response.nil?
+        if @client.integration_name == 'pet_premium'
+          if response["Response"]["Result"]["Value"] == "BaeOK"
+            sold = true
+            break
+          end
+        elsif @client.integration_name == 'pet_first'
+          if response["Error"]["ErrorText"] == ""
+            sold = true
+            break
+          end
+        end
+        
+      end
 
       count += 1
     end
 
-    unless response.nil?
+    if sold
       Response.create(response: response.to_s, lead_id: lead.id, client_name: @client.integration_name)
       if @client.integration_name == "pet_first"
         ResponsePetfirstWorker.perform_async(lead_id)
