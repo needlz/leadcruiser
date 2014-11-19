fe469e9203092cf981bbf419044a95df047f332c
rorfan8782 <knhsinbad8782@gmail.com>
2014-11-19 16:30:42 +0100

    Updated Mandrill credentials

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -4,7 +4,7 @@ unless Rails.env.test?
   ActionMailer::Base.smtp_settings = {
       address:   'smtp.mandrillapp.com',
       port:      587,
-      user_name: 'pavlo.vavruk@gmail.com',
-      password:  'AWf4dDGf9L'
+      user_name: 'rnatsch@gmail.com',
+      password:  'presidio1presidio'
   }
 end
