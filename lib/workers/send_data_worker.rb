9ccf7d8ea21e3f45c8e859ac7649db5dcc28c66c
rorfan8782 <knhsinbad8782@gmail.com>
2014-12-09 20:01:02 +0100

    Add state filter for Pets Best client

--- lib/workers/send_data_worker.rb
+++ lib/workers/send_data_worker.rb
@@ -21,6 +21,17 @@ class SendDataWorker
       end
       builder = NextClientBuilder.new(lead, client_verticals)
       @client = ClientsVertical.where(active: true, integration_name: builder.integration_name).first
+
+      # TODO: Replace here with Puchase Order filter
+      if @client.integration_name == ClientsVertical::PETS_BEST
+        state_filter = ["CA", "NY", "TX", "CO", "FL", "NJ", "AZ", "NV", "IL", "VA"]
+        state = lead.state || lead.try(:zip_code).try(:state)
+        unless state_filter.include? state
+          count += 1
+          next
+        end
+      end
+
       provider = DataGeneratorProvider.new(lead, @client)
 
       response = provider.send_data
