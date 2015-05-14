ff32927d42d90c0a50c5ea53ce6daca4d55de262
rorfan8782 <knhsinbad8782@gmail.com>
2015-05-14 22:35:42 +0200

    Change timezone for reports page

--- app/logic/reporting/lead_statistics.rb
+++ app/logic/reporting/lead_statistics.rb
@@ -29,11 +29,14 @@ module Reporting
     private
 
     def period(first_date, last_date)
-      Time.zone = "Pacific Time (US & Canada)"
-      from = first_date.nil? || first_date == "" ? 14.days.ago : Time.zone.parse(first_date).try(:beginning_of_day)
-      to = last_date.nil? || last_date == "" ? Time.zone.now : Time.zone.parse(last_date).try(:end_of_day)
-      from = from.in_time_zone("UTC")
-      to = to.in_time_zone("UTC")
+      # Time.zone = "Pacific Time (US & Canada)"
+      # from = first_date.nil? || first_date == "" ? 14.days.ago : Time.zone.parse(first_date).try(:beginning_of_day)
+      # to = last_date.nil? || last_date == "" ? Time.zone.now : Time.zone.parse(last_date).try(:end_of_day)
+      # from = from.in_time_zone("UTC")
+      # to = to.in_time_zone("UTC")
+      from = first_date.nil? || first_date == "" ? 14.days.ago : Time.parse(first_date).try(:beginning_of_day)
+      to = last_date.nil? || last_date == "" ? Time.now : Time.parse(last_date).try(:end_of_day)
+
       from..to
     end
 
