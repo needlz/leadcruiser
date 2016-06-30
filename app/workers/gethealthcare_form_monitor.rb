22b00cae4a5971d4f26bb8f47ed829078426b898
RomanSokhan <romans@softwareplanetgroup.com>
2016-06-30 14:58:01 +0300

    Fixes

--- app/workers/gethealthcare_form_monitor.rb
+++ app/workers/gethealthcare_form_monitor.rb
@@ -1,22 +1,58 @@
 require 'capybara'
 require 'capybara/dsl'
 
+module WaitForAjax
+  def wait_for_ajax
+    Timeout.timeout(Capybara.default_max_wait_time) do
+      loop until finished_all_ajax_requests?
+    end
+  end
+
+  def finished_all_ajax_requests?
+    page.evaluate_script('jQuery.active').zero?
+  end
+end
+
 class GethealthcareFormMonitor
   include Sidekiq::Worker
   sidekiq_options queue: "high"
 
   include Capybara::DSL
+  include WaitForAjax
 
   def perform
-
-    hit_server
+    begin
+      hit_server
+    rescue Capybara::ElementNotFound => e
+      if Rails.env.development?
+        save_screenshot('/screens/file.png')
+        p page.current_url
+        p e
+        p page.body
+      else
+        raise e
+      end
+    end
     delay_minutes = EditableConfiguration.global.gethealthcare_form_monitor_delay_minutes
     GethealthcareFormMonitor.perform_in(delay_minutes.minutes)
   end
 
   private
 
+  def wait_until(&block)
+    time_elapsed = 0
+    start_time = Time.now
+    done = false
+    until time_elapsed > Capybara.default_max_wait_time || done
+      done = block.call
+      time_elapsed = Time.now - start_time
+      sleep 0.5
+    end
+    done
+  end
+
   def hit_server
+    Capybara.reset_sessions!
     page.visit("/")
 
     hit = GethealthcareHit.new
@@ -32,44 +68,47 @@ class GethealthcareFormMonitor
 
   def submit_form
     #step_1
-    find('.maleRadio').click
+    find('.maleRadio').trigger("click")
     find('.age input').set('12/30/1994')
     find('.zipcode input').set('01001')
-    find('.startBtnBox input').click
+    find('.startBtnBox input').trigger("click")
 
-    #step_2
-    sleep 2
+    #step 1a "Would you like to add a family member?"
     switch_to_window windows.last
-    find('a.nextBtn1a').click
+    find('a.nextBtn1a').trigger("click")
 
-    #step_3
-    sleep 2
-    find('.noneCheck').click
-    find('.nextBtn').click
+    #step 2 "Any Recent Life Events?"
+    find('.noneCheck').trigger("click")
+    find('.nextBtn').trigger("click")
 
-    #step_4
-    sleep 2
-    find('.nextBtn').click
+    #step 2a "We need some information about your household to find you the right plans."
+    find('.nextBtn').trigger("click")
+    sleep 1
+    find('.nextBtn').trigger("click")
 
-    #step_5
-    sleep 2
+    #step 2b "Okay, tell us more about your household."
     find('#step2b-height_ft').set('5')
     find('#step2b-height_in').set('2')
     find('#step2b-weight').set('170')
-    find('.personConditions .noNo').click
-    find('.personTobacco .noNo').click
-    find('.nextBtnStep2b').click
+    find('.personConditions .noNo').trigger("click")
+    find('.personTobacco .noNo').trigger("click")
+    find('.nextBtnStep2b').trigger("click")
 
     #step_6
-    sleep 2
     find('#step3-firstname').set('test')
     find('#step3-lastname').set('test')
     find('#step3-email').set('example@test.com')
     find('#step3-phone').set('7871111111')
     find('#step3-address1').set('adress1')
-    find('.seePlansNow').click
-    pp page.driver.error_messages if Capybara.current_driver == :webkit_debug
-    sleep 5
-    switch_to_window windows.last
+    find('.seePlansNow').trigger("click")
+
+    wait_until do
+      p page.current_url
+      page.current_url.start_with?('http://gethealthcare.co/thanks-page')
+    end
+    wait_until do
+      p page.current_url
+      page.current_url != 'http://gethealthcare.co/next-steps'
+    end
   end
 end
