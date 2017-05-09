4d5a3b63812a79cf8b9a1a42da558c72f8b927a8
RomanS <romans@softwareplanetgroup.com>
2017-05-09 11:54:56 +0300

    Support for filters

--- app/logic/forward_health_insurance_lead.rb
+++ app/logic/forward_health_insurance_lead.rb
@@ -21,17 +21,17 @@ class ForwardHealthInsuranceLead
   def forward_to(integration_names, options = {})
     purchase_orders = Vertical.health_insurance.purchase_orders.active.
       joins(:clients_vertical).where(clients_verticals: { integration_name: integration_names })
-    forward_purchace_orders(purchase_orders, options)
+    forward_purchase_orders(purchase_orders, options)
   end
 
   def forward_to_all_except(integration_names)
     purchase_orders = Vertical.health_insurance.purchase_orders.active.
       joins(:clients_vertical).where.not(clients_verticals: { integration_name: integration_names })
-    forward_purchace_orders(purchase_orders)
+    forward_purchase_orders(purchase_orders)
   end
 
-  def forward_purchace_orders(purchace_orders, options = {})
-    purchace_orders.each do |purchase_order|
+  def forward_purchase_orders(purchase_orders, options = {})
+    filtered_purchase_orders(purchase_orders).each do |purchase_order|
       client = purchase_order.clients_vertical
 
       next if client.boberdoo? && ForwardingTimeRange.inside_afterhours_range?
@@ -40,12 +40,31 @@ class ForwardHealthInsuranceLead
     end
   end
 
+  def filtered_purchase_orders(purchase_orders)
+    state = lead.state || lead.try(:zip_code).try(:state)
+    purchase_orders.select do |purchase_order|
+      if purchase_order.states.present?
+        states = purchase_order.states_array
+        states.map(&:strip!)
+        next if states.present? and !states.include?(state)
+      end
+
+      next if boolean?(purchase_order.preexisting_conditions) &&
+        purchase_order.preexisting_conditions != (lead.health_insurance_lead.preexisting_conditions == 'yes')
+      true
+    end
+  end
+
+  def boolean?(value)
+    value.kind_of?(TrueClass) || value.kind_of?(FalseClass)
+  end
+
   def forwardable_to_ICD?
     lead.health_insurance_lead.lead_type == 'Health Insurance' && lead.health_insurance_lead.src == 'HealthMatchup'
   end
 
   def forward_to_client_delay_aware(client, purchase_order, options = {})
-    delay_for_client = options[:delay_seconds] || client.lead_forwarding_delay_seconds
+    delay_for_client = client.lead_forwarding_delay_seconds
     if delay_for_client > 0
       ForwardLeadToClientRequest.perform_in(delay_for_client, lead.id, purchase_order.id)
     else
